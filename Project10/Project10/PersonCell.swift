50a57c765a2e5db59ba4ec16c68b95f8b885df0f
Clay <cdnp44@gmail.com>
2021-07-21 15:57:44 -0400

    Add files via upload

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Clayton DeWitt on 7/12/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var name: UILabel!
+    
+    
+}
